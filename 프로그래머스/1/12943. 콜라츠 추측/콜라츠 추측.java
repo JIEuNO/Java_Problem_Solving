f9a253c0eac4021611674c9b6fb03dac0966f3ee
JIEuNO <46878861+JIEuNO@users.noreply.github.com>
2024-05-13 21:04:09 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        
+        while(num != 1 && answer < 500) {
+            num = (num % 2 == 0) ? (num / 2) : (num * 3 + 1);
+            answer += 1;
+        }
+        
+        if (answer >= 500)
+            answer = -1;
+        
+        return answer;
+    }
+}
