bad9f4d197ac46f42beb077a34eb04d498abcb48
JIEuNO <46878861+JIEuNO@users.noreply.github.com>
2024-07-18 22:32:32 +0900

    [level 2] Title: 오픈채팅방, Time: 151.69 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -4,26 +4,63 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    public List<String> solution(String[] record) {
+    public String[] solution(String[] record) {
         int len = record.length;
-        String[] str;
-        List<String> answer = new ArrayList<>();
-        Map<String, String> name = new HashMap<>();
+        char c;
+        String[] str, answer;
+        List<Command> command = new ArrayList<>();
+        Map<String, User> name = new HashMap<>();
         
         for (int i = 0; i < len; i++) {
             str = record[i].split(" ");
-            if (!str[0].equals("Leave")) 
-                name.put(str[1], str[2]);
+            c = str[0].charAt(0);
+            if (c == 'E') {
+                if (!name.containsKey(str[1]))
+                    name.put(str[1], new User(str[1], str[2]));
+                else
+                    name.get(str[1]).nickname = str[2];
+                command.add(new Command(c, str[1]));
+            }
+            else if (c == 'L') {
+                command.add(new Command(c, str[1]));
+            }
+            else if (c == 'C') {
+                name.get(str[1]).nickname = str[2];
+            }
         }
         
-        for (int i = 0; i < len; i++) {
-            str = record[i].split(" ");
-            if (str[0].equals("Enter"))
-                answer.add(name.get(str[1]) + "님이 들어왔습니다.");
-            else if (str[0].equals("Leave"))
-                answer.add(name.get(str[1]) + "님이 나갔습니다.");
-        }
+        answer = new String[command.size()];
+        for (int i = 0; i < command.size(); i++) {
+            String s;
+            if (command.get(i).c == 'E')
+                s = "님이 들어왔습니다.";
+            else
+                s = "님이 나갔습니다.";
+            answer[i] = name.get(command.get(i).uid).nickname + s;
+        }  
         
         return answer;
     }
+}
+
+class User {
+    
+    String uid, nickname;
+    
+    User (String uid, String nickname) {
+        this.uid = uid;
+        this.nickname = nickname;
+    }
+}
+
+class Command {
+    
+    char c;
+    String uid;
+    
+    Command (char c, String uid) {
+        this.c = c;
+        this.uid = uid;
+    }
+    
 }
